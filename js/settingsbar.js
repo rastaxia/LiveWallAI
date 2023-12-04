c05db595f7b4a3f4d48de8d22ca3dafe8373067a
Regter,Jeanine J.E.M.C. de <j.deregter@student.fontys.nl>
2023-12-04 16:06:42 +0100

    Change fontsize works. Have to implent in popover

--- js/settingsbar.js
+++ js/settingsbar.js
@@ -1,5 +1,23 @@
+
 // set width of input with placeholder
 let input = document.querySelectorAll('input');
-for(i=0; i<input.length; i++){
+for (i = 0; i < input.length; i++) {
     input[i].setAttribute('size', input[i].getAttribute('placeholder').length);
 }
+
+let text = document.querySelector('p');
+
+// let text = document.getElementsByClassName('text1');
+
+let textFontSize = 16;
+
+increaseBtn = document.getElementById('increase'),
+    decreaseBtn = document.getElementById('decrease');
+increaseBtn.addEventListener('click', () => {
+    textFontSize += 1
+    text.style.fontSize = `${textFontSize}px`
+})
+decreaseBtn.addEventListener('click', () => {
+    textFontSize -= 1
+    text.style.fontSize = `${textFontSize}px`
+})
