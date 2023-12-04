10d869842dbc8991cc039b8d42de53e218145916
Regter,Jeanine J.E.M.C. de <j.deregter@student.fontys.nl>
2023-12-04 20:47:32 +0100

    Made fontsize and lineheight adjustable

--- js/settingsbar.js
+++ js/settingsbar.js
@@ -1,23 +1,42 @@
-
 // set width of input with placeholder
 let input = document.querySelectorAll('input');
 for (i = 0; i < input.length; i++) {
     input[i].setAttribute('size', input[i].getAttribute('placeholder').length);
 }
 
-let text = document.querySelector('p');
+// change fontsize in pop up
+let text = document.querySelector('p');  // selected paragraph to change fontsize
 
-// let text = document.getElementsByClassName('text1');
+let textFontSize = 20; // Start number of fontsize. Is the same as p fontsize
 
-let textFontSize = 16;
-
-increaseBtn = document.getElementById('increase'),
-    decreaseBtn = document.getElementById('decrease');
+increaseBtn = document.getElementById('increasefontSize'),
+    decreaseBtn = document.getElementById('decreasefontSize');  // get id of buttons change fontsize
 increaseBtn.addEventListener('click', () => {
-    textFontSize += 1
-    text.style.fontSize = `${textFontSize}px`
+    textFontSize += 1 // fontsize +1
+    text.style.fontSize = `${textFontSize}px`;
+    document.getElementsByName('fontSize')[0].placeholder = text.style.fontSize = `${textFontSize}`; // fontsize shown in placeholder
 })
 decreaseBtn.addEventListener('click', () => {
-    textFontSize -= 1
+    textFontSize -= 1 // fontsize -1
     text.style.fontSize = `${textFontSize}px`
+    document.getElementsByName('fontSize')[0].placeholder = text.style.fontSize = `${textFontSize}`; // fontsize shown in placeholder
 })
+
+// Change line-height
+text = document.querySelector('p');  // selected paragraph to change fontsize
+
+let textLineheight = 1.5;
+
+increaseBtn2 = document.getElementById('increaselineHeight'),
+    decreaseBtn2 = document.getElementById('decreaselineHeight');  // get id of buttons change lineheight
+increaseBtn2.addEventListener('click', () => {
+    textLineheight += 0.5 // lineheight +1
+    text.style.lineHeight = `${textLineheight}`;
+    document.getElementsByName('lineHeight')[0].placeholder = text.style.lineHeight = `${textLineheight}`; // lineheight shown in placeholder
+})
+decreaseBtn2.addEventListener('click', () => {
+    textLineheight -= 0.5 // lineheight -1
+    text.style.lineHeight = `${textLineheight}`;
+    document.getElementsByName('lineHeight')[0].placeholder = text.style.lineHeight = `${textLineheight}`; // lineheight shown in placeholder
+})
+
