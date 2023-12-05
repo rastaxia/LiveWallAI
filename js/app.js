fd17dc07cc03b33b475631880c45888cc166a5ea
Rob <505067@student.fontys.nl>
2023-12-05 10:39:33 +0100

    added .evn wip random story

--- js/app.js
+++ js/app.js
@@ -2,16 +2,15 @@
 const express = require("express");
 const OpenAI = require("openai");
 const path = require("path");
-const bodyParser = require("body-parser"); // Add this line
+const bodyParser = require("body-parser");
 
-// Set the OpenAI API key as an environment variable
-process.env.OPENAI_API_KEY = "no leak";
-
+require("dotenv").config();
 // Create an instance of the OpenAI class
 const openai = new OpenAI();
 
 // Create an Express application
 const app = express();
+let generated = false;
 
 // Define the port to listen on, using the specified port or defaulting to 3000
 const port = process.env.PORT || 3000;
@@ -24,30 +23,106 @@ app.use(bodyParser.json());
 
 //Data handeling
 app.post("/create-fairy-tale.html", async (req, res) => {
+  // gets data from client
   const { name, local, world } = req.body;
   try {
+    // creates the story
     const chat = await openai.chat.completions.create({
       messages: [
         {
           role: "system",
-          content: `tell me a story using name ${name}, local ${local}, world ${world} put it in a JSON format the story must be 2 lines long`,
+          content: `
+          (variabel 1), (variabel 2), (variabel 3) controleer in elke denkbare context of deze variabelen aanstootgevend, obsceen, seksueel, racistisch of gewelddadig zijn voor kinderen.
+          Als de variabelen kindvriendelijk zijn, schrijf dan een fantasieverhaal in het NEDERLANDS waar de hoofdpersonage ${name} heet. Dit verhaal moet een hero's journey hebben; De hoofdpersoon leefde in een ${local} 
+          houd rekening met dat de wereld ${world} bevat. 
+          Geef het verhaal ook een titel De titel moet altijd boven het verhaal staan onder de keywords. Het verhaal moet minimaal 2000 woorden bevatten. Je hoeft niet te vermelden of het verhaal kindvriendelijk is;
+          je kunt direct met het verhaal beginnen. Als dit niet het geval is, genereer dan alleen de woorden NIET KINDVRIENDELIJK. Genereer ook 3 keywords; laat de keywords slechts 1 woord lang zijn.
+          Deze keywords moeten gebruikt kunnen worden om 1 foto per keyword te zoeken die goed bij het verhaal past. Beschrijf de keywords als eerste bovenaan het verhaal voordat de titel wordt genoemd;
+          DOE HET ALTIJD ALS VOLGT: Keywords: keyword1, keyword2, keyword3.!!!! er hoeft geen comma , na de laatste keyword. De keywords moeten geen namen zijn van de karakters.
+          Je hoeft niet te vermelden wat het doel van het verhaal was ook hoef je niet te zeggen story:. Maak de keywords in het ENGELS!!. Het vehraal zelf moet in het NEDERLANDS zijn.
+          Zet ook op drie verschillende plekken in het verhaal waar een foto moet doe dit door te zeggen foto1 foto2 foto3 het verhaal mag dus maar 3 fotos hebbne. Zet de output in een JSON formaat.`,
         },
         {
           role: "user",
-          content: "Once upon a time",
+          content: `Variabel 1 = ${name} ; Variabel 2 = ${local} ; Variabel 3 = ${world}`,
         },
       ],
-      model: "gpt-3.5-turbo-1106",
+      model: "gpt-4-1106-preview",
       temperature: 0.6,
       stream: true,
       response_format: { type: "json_object" },
+      presence_penalty: 0,
+      frequency_penalty: 0,
     });
 
     for await (const chunk of chat) {
-      res.json(chunk.choices[0].delta.content);
+      // only writes the chunks that are not empty or undefined
+      if (
+        chunk.choices[0].delta.content !== undefined &&
+        chunk.choices[0].delta.content !== null
+      ) {
+        res.write(chunk.choices[0].delta.content);
+      }
+      // stops the stream
+      if (chunk.choices[0].finish_reason === "stop") {
+        res.end();
+      }
     }
   } catch (err) {
     console.log(err);
+  }
+});
+
+app.post("/random", async (req, res) => {
+  const random = await openai.answers.create({
+    messages: [
+      {
+        role: "system",
+        content: `Schrijf een fantasieverhaal in het NEDERLANDS die 2000 woorden lang is dit verhaal moet een hero's journy hebben en 3 keywords bevatten. De keywords moeten 1 woord lang zijn. en moet gebruikt kunnen wordn om fotos te vinden`,
+      },
+    ],
+    model: "gpt-4-1106-preview",
+    temperature: 0.6,
+    stream: true,
+    response_format: { type: "json_object" },
+    presence_penalty: 0,
+    frequency_penalty: 0,
+  });
+});
+
+// Image generation
+app.post("/imgGenerate", async (req, res) => {
+  // gets data from client
+  const { keywords } = req.body;
+
+  // checks if the images are already generated
+  if (!generated) {
+    generated = true;
+    // use dall-e-3 for better quality
+    const img1 = await openai.images.generate({
+      model: "dall-e-2",
+      prompt: `generate me a fantasy image of ${keywords[0]}`,
+      n: 1,
+      size: "256x256",
+    });
+    const img2 = await openai.images.generate({
+      model: "dall-e-2",
+      prompt: `generate me a fantasy image of ${keywords[1]}`,
+      n: 1,
+      size: "256x256",
+    });
+    const img3 = await openai.images.generate({
+      model: "dall-e-2",
+      prompt: `generate me a fantasy image of ${keywords[2]}`,
+      n: 1,
+      size: "256x256",
+    });
+    // sends the images to the client
+    res.json({
+      img1: img1.data[0].url,
+      img2: img2.data[0].url,
+      img3: img3.data[0].url,
+    });
   }
 });
 
--- js/script.js
+++ js/script.js
@@ -5,11 +5,19 @@ const worldInput = document.querySelector("#magicWorld");
 const start = document.querySelector("#startStory");
 const random = document.querySelector("#randomButton");
 const outputDiv = document.querySelector("#output");
+let called = false;
 let keywords = [];
 
 random.addEventListener("click", function (event) {
   event.preventDefault();
   console.log("random button clicked");
+  fetch("/random", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
 });
 
 // Add a submit event listener to the form
@@ -33,13 +41,110 @@ start.addEventListener("click", async function (event) {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ name, local, world }),
+        body: JSON.stringify({ name, local, world, keywords }),
       });
+      //gets data from server
+      const reader = response.body.getReader();
+      return new ReadableStream({
+        start(controller) {
+          // The following function handles each data chunk
+          function push() {
+            // "done" is a Boolean and value a "Uint8Array"
+            reader.read().then(({ done, value }) => {
+              // If there is no more data to read
+              if (done) {
+                console.log("done", done);
+                controller.close();
+                return;
+              }
+              // Get the data and send it to the browser via the controller
+              controller.enqueue(value);
+              // Check chunks by logging to the console
+              value = new TextDecoder().decode(value);
+              let totalContent = (outputDiv.innerHTML + value)
+                .replace(
+                  /\\n|([^.]|^)\n([^.]|$)|\n+|(?!\.)\n(?!\.)/g,
+                  (_, a, b, c) => (a ? "<br/>" : c ? "<br/>" : "")
+                )
+                .replace(/['"{}}]/g, "")
+                .replace("undefined", "")
+                .replace(
+                  /(foto1)|(Foto1)/g,
+                  "<img id='photo1' src='#' alt='Photo 1' >"
+                )
+                .replace(
+                  /(foto2)|(Foto2)/g,
+                  "<img id='photo2' src='#' alt='Photo 2' >"
+                )
+                .replace(
+                  /(foto3)|(Foto3)/g,
+                  "<img id='photo3' src='#' alt='Photo 3' >"
+                );
+              outputDiv.innerHTML = totalContent;
 
-      const responseData = await response.json();
-      console.log(responseData);
+              // Extracting keywords from the content
+              const keywordsMatch = totalContent.match(/Keywords: (.+?)\./i);
+              let extractedKeywords = [];
+              if (keywordsMatch && keywordsMatch.length > 1) {
+                extractedKeywords = keywordsMatch[1].split(", ");
+              }
+
+              // Ensure only three keywords are considered
+              const finalKeywords = extractedKeywords
+                .slice(0, 3)
+                .map((keyword) =>
+                  keyword
+                    .split("<br>")[0]
+                    .replace(/<[^>]*>/g, "")
+                    .trim()
+                );
+
+              // Store the final keywords for later use
+              keywords = finalKeywords;
+              if (keywords.length === 3) {
+                if (!called) {
+                  called = true;
+                  imgGenerate();
+                }
+              }
+              push();
+            });
+          }
+          push();
+        },
+      });
     } catch (err) {
       console.log(err);
     }
   }
 });
+
+// img gen request
+async function imgGenerate() {
+  const img = await fetch("/imgGenerate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ keywords }),
+  });
+  const imgData = await img.json();
+  let photo1_polling = setInterval(() => {
+    if (document.querySelector("img#photo1") !== null) {
+      document.querySelector("img#photo1").src = imgData.img1;
+      clearInterval(photo1_polling);
+    }
+  }, 1000);
+  let photo2_polling = setInterval(() => {
+    if (document.querySelector("img#photo2") !== null) {
+      document.querySelector("img#photo2").src = imgData.img2;
+      clearInterval(photo2_polling);
+    }
+  }, 1000);
+  let photo3_polling = setInterval(() => {
+    if (document.querySelector("img#photo3") !== null) {
+      document.querySelector("img#photo3").src = imgData.img3;
+      clearInterval(photo3_polling);
+    }
+  }, 1000);
+}
